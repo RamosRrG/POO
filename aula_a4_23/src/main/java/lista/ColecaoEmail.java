412d206ac9aa499a6c79dfe35f0fdc4fdf2e9dee
Gustavo Ramos ROcha <gustavo.r16@aluno.ifsc.edu.br>
2026-04-29 22:06:46 -0300

    fiz a classe colecao email e iniciei a contato na aula 23-4

--- aula_a4_23/src/main/java/lista/ColecaoEmail.java
+++ aula_a4_23/src/main/java/lista/ColecaoEmail.java
@@ -11,12 +11,54 @@ public class ColecaoEmail {
 
     public boolean adicionarEmail(String key, String valor){
         String eR = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
+        boolean c = false;
+        if (valor.matches(eR)){
+            if(!dados.containsKey(key)){
+                dados.put(key,valor);
+                c = true;
+            }
+        }
+        return c;
+    }
 
+    public boolean removerEmail(String key){
+        if(dados.containsKey(key)){
+            dados.remove(key);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean updateEmail(String key, String valor){
+        if(dados.containsKey(key)){
+            dados.put(key,valor);
+            return true;
+        }else{
+            return false;
+        }
     }
 
 
 
+    public String formata(String mascara, String valor){
+        MaskFormatter mask = null;
+        String resultado = "";
+        try {
+            mask = new MaskFormatter(mascara);
+            mask.setValueContainsLiteralCharacters(false);
+            mask.setPlaceholderCharacter('_');
+            resultado = mask.valueToString(valor);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return resultado;
+    }
 
-
-
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        this.dados.forEach((String c, String v)-> sb.append(c).append(" : ").append(v).append("/n"));
+        return sb.toString();
+    }
 }
--- aula_a4_23/src/main/java/lista/Contato.java
+++ aula_a4_23/src/main/java/lista/Contato.java
@@ -1,4 +1,39 @@
 package lista;
 
+import java.time.LocalDate;
+
 public class Contato {
+    private String nome;
+    private String sobrenome;
+    private LocalDate dataNasc;
+    private ColecaoTelefone telefones;
+    private ColecaoEmail emails;
+
+    public Contato(String nome, String sobrenome, LocalDate dataNasc){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.dataNasc = dataNasc;
+        this.telefones = new ColecaoTelefone();
+        this.emails = new ColecaoEmail();
+    }
+
+    public boolean adicionaEmails(String rotulo, String email){
+        return this.emails.adicionarEmail(rotulo, email);
+    }
+    public boolean removeTelefone(String rotulo){
+        return this.telefones.removerNumero(rotulo);
+    }
+    public boolean removeEmails(String rotulo) {
+        return this.emails.removerEmail(rotulo);
+    }
+    public boolean updateNumero(String rotulo, String numero){
+        return this.telefones.updateNumero(rotulo, numero);
+    }
+    public boolean updateEmail(String rotulo, String email){
+        return this.emails.updateEmail(rotulo, email);
+    }
+    
+
+
+
 }
