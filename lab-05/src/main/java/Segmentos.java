4414660aaee6f1d3d7fef3bca18a1482aa4f0b3b
RamosRrG <gustavo.r16@aluno.ifsc.edu>
2026-05-29 19:57:57 -0300

    testes da classe algs

--- lab-05/src/main/java/Segmentos.java
+++ lab-05/src/main/java/Segmentos.java
@@ -1,31 +1,62 @@
 import edu.princeton.cs.algs4.Draw;
-import java.util.concurrent.TimeUnit;
 
-public class Segmentos {
+import java.awt.Color;
 
-            public static void main(String[] args) throws Exception {
-                Draw desenho = new Draw();
-                desenho.setCanvasSize(800, 800);
+public class Segmentos{
+    public static void main(String[] args){
+        Draw desenho = new Draw();
+        int dimensao = 800;
+        desenho.setCanvasSize(dimensao, dimensao);
+        desenho.setXscale(0, dimensao);
+        desenho.setYscale(0, dimensao);
+        double fator = 200;
+        double fatorCor = 0.2;
+        Color clara = Draw.GREEN;
+        Color escura = new Color((int)(clara.getRed()*fatorCor),
+                (int)(clara.getGreen()*fatorCor),
+                (int)(clara.getBlue()*fatorCor));
+        desenho.enableDoubleBuffering();
+        desenho.clear(Draw.WHITE);
 
-                desenho.setXscale(0, 800);
-                desenho.setYscale(0, 800);
+// Desenhando grade quadriculada
+        int grade = (int) fator/10;
+        desenho.setPenColor(Draw.LIGHT_GRAY);
+        for (int i = 0; i <= dimensao; i += grade) {
+            desenho.line(i, 0, i, dimensao); // linha vertical
+            desenho.line(0, i, dimensao, i); // linha horizontal
+        }
+        double xInicial = 300;
+        double yInicial = 400;
+// Montando vetores com os pontos em X e em Y para desenhar um segmento horizontal
+        String posicao = IO.readln();
+        if(posicao.equals("horizontal")) {
 
-// Toda ação de desenho acontecerá em um buffer secundário
-// e esse buffer só será visto depois que for invocado o método show()
 
-                desenho.enableDoubleBuffering();
-                for (int i = 1; i ; i++) {
-
-                    desenho.clear();
-// escrevendo o valor de i na coordenada (500,500)
-
-                    desenho.text(500, 500, "" + i);
-// Trocando o buffer para exibir o que foi escrito
-
-                    desenho.show();
-
-// Dormindo por 1 segundo
-                    TimeUnit.SECONDS.sleep(1);
-                }
+            yInicial = 180;
+            double[] xHorizontal = {0.1 * fator + xInicial, 0.2 * fator + xInicial, 1.0 * fator + xInicial, 1.1 * fator + xInicial, 1.0 * fator +
+                    xInicial, 0.2 * fator + xInicial};
+            double[] yHorizontal = {0.2 * fator + yInicial, 0.3 * fator + yInicial, 0.3 * fator + yInicial, 0.2 * fator + yInicial, 0.1 * fator +
+                    yInicial, 0.1 * fator + yInicial};
+            // Desenhando o segmento horizontal
+            desenho.setPenColor(clara);
+            desenho.filledPolygon(xHorizontal, yHorizontal);
+        }else{
+            // Montando vetores com os pontos em X e em Y para desenhar um segmento vertical
+            yInicial = 200;
+            double[] xVertical = {0.1*fator+xInicial, 0.2*fator+xInicial, 0.2*fator+xInicial, 0.1*fator+xInicial, 0*fator+
+                    xInicial, 0*fator+xInicial};
+            double[] yVertical = {0.2*fator+yInicial, 0.3*fator+yInicial, 1.0*fator+yInicial, 1.1*fator+yInicial, 1.0*fator+
+                    yInicial, 0.3*fator+yInicial};
+            // Desenhando o segmento vertical
+            desenho.setPenColor(escura);
+            desenho.filledPolygon(xVertical, yVertical);
+            // Desenhando outro segmento vertical, porém com x deslocado em 'fator' pixels
+            for (int i = 0; i < xVertical.length; i++) {
+                xVertical[i]+=fator;
             }
-}
+            desenho.filledPolygon(xVertical, yVertical);
+        }
+// Trocando o buffer para exibir o que foi desenhado
+        desenho.show();
+    } // fim do main
+}// fim da classe
